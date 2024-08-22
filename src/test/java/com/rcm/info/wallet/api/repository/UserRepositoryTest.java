118b8738f36a066901dbe3cbce00308c0bc10d83
= <=>
2024-08-22 11:36:07 -0300

    finalizado o repositório do usuário

--- src/test/java/com/rcm/info/wallet/api/repository/UserRepositoryTest.java
+++ src/test/java/com/rcm/info/wallet/api/repository/UserRepositoryTest.java
@@ -1,29 +1,57 @@
 package com.rcm.info.wallet.api.repository;
 
 import com.rcm.info.wallet.api.entity.User;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserRepositoryTest {
 
+    private static final String EMAIL = "email@email.com";
+
     @Autowired
     UserRepository userRepository;
 
-    @Test
-    public void testSave(){
+    @Before
+    public void setUp() {
         User user = new User();
         user.setName("Fernanda");
         user.setPassword("123");
-        user.setEmail("fernanda@email.com");
+        user.setEmail("email@email.com");
+        userRepository.save(user);
+    }
+
+    @After
+    public void tearDown() {
+        userRepository.deleteAll();
+    }
+
+
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setName("Fernanda");
+        user.setPassword("123");
+        user.setEmail("email@email.com");
 
         User response = userRepository.save(user);
         assertNotNull(response);
+    }
+
+    @Test
+    public void testFindByEmail() {
+        Optional<User> response = userRepository.findByEmailEquals(EMAIL);
+        assertTrue(response.isPresent());
+        assertEquals(response.get().getEmail(), EMAIL);
     }
 }
